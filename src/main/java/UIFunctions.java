808fe9820fcc2e7260176318dced08690ad9f4a8
Albin Bystrom <albinby@kth.se>
2017-10-23 16:27:51 +0800

    UI functions

---
+++ src/main/java/UIFunctions.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+
+/**
+ * Created by Albin on 2017-10-23.
+ */
+public class UIFunctions {
+    public static void clear() {
+        try
+        {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows"))
+            {
+                Runtime.getRuntime().exec("cls");
+            }
+            else
+            {
+                Runtime.getRuntime().exec("clear");
+            }
+        }
+        catch (final Exception e)
+        {
+            //  Handle any exceptions.
+        }
+    }
+
+    public static void waitForUser(){
+        System.out.println("Press enter to continue...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void divder() {
+        System.out.println("========================================");
+    }
+
+
+}
