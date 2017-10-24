3a85325ab8a0cb7c750d41b05a3afb4fef1241e6
dswk95 <32730553+dswk95@users.noreply.github.com>
2017-10-24 02:36:32 -0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,16 +1,33 @@
 /**
  * Created by Albin on 2017-10-12.
 */
+/**
+ * Created by Albin on 2017-10-12.
+*/
 import java.text.SimpleDateFormat;
+import java.io.*;
 import java.util.*;
 
-
 public class Driver {
-
-    public static void main(String args[]){
+static List<Movie> movies = new ArrayList<Movie>();
     
+public static void main(String args[]){
+    
+		String title;
+	    String director;
+	    int duration;
+	    int castno;
+	    String[] cast;
+	    int option;
+	    Type type;
+	    Date startDate;
+	    Date endDate;
+	    int year;
+	    int month; 
+	    int date;			//for new movie insertion
+	    String a;
+	    
     	Scanner sc = new Scanner(System.in);
-    	
     	String[] users = {"user123", "123"}; 		//will be implemented with file IO
     	String user,password;
     	while(true){
@@ -34,8 +51,47 @@ public class Driver {
     	System.out.println("5 - Quit");
     	choice = sc.nextInt();
     	switch(choice){
-    		case 1: break;				//Change ticket price 
-    		case 2: break;				//enter movie
+    		case 1: 
+    		{	System.out.println("1 - Change Ticket Prices");
+    			System.out.println("2 - Add/Remove Holidays");
+    			break;}				//Change ticket price 
+    		case 2:	{
+    			System.out.println("Enter Movie Title: ");
+    			a = sc.nextLine();
+    			title = sc.nextLine();
+    			System.out.println("Enter Director Name: ");
+    			director = sc.nextLine();
+    			System.out.println("Enter Movie Duration (in minutes): ");
+    			duration = sc.nextInt();
+    			System.out.println("Enter the number of cast members ");
+    			castno = sc.nextInt();
+    			cast = new String[castno];
+    			System.out.println("Enter cast name");
+    			a = sc.nextLine();
+    			for(int x=0;x<castno;x++){
+    				cast[x] = sc.nextLine();
+      			}
+    			System.out.println("Enter Movie Type");
+    			System.out.println("1 - 3D\n2 - Digital");
+    			option = sc.nextInt();
+    			if(option==1){
+    				type = Type.TREED;
+    			}else{
+    				type = Type.DIGITAL;
+    			}
+    			System.out.println("Enter the start date in the format DD MM YYYY");
+    			date = sc.nextInt();
+    			month = sc.nextInt();
+    			year = sc.nextInt();
+    			startDate = new Date(year,month,date);
+    			System.out.println("Enter the end date in the format DD MM YYYY");
+    			date = sc.nextInt();
+    			month = sc.nextInt();
+    			year = sc.nextInt();
+    			endDate = new Date(year,month,date);
+    			Movie test = new Movie(title,director,duration,cast,type,startDate,endDate);    			
+    			System.out.println(test.toString());
+    			break;}		//enter movie
     		case 3: break;				//update the details of the movies or remove the movie
     		case 4:	break;				//by sales or rating
     		case 5: break;				//quit
@@ -45,10 +101,12 @@ public class Driver {
     		}
     	
     	}
-    
+  
+    	
+    	
 }
 
-    
+	
     
     	
     }
