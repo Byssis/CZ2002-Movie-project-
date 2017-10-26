bef648da2577a3f82e885c0fc0e60a01f6c716cd
manzaigit <chanyihao@Chans-MacBook-Pro.local>
2017-10-26 14:58:23 +0800

    modified code for clearing screen

--- src/main/java/UIFunctions.java
+++ src/main/java/UIFunctions.java
@@ -7,15 +7,31 @@ public class UIFunctions {
     public static void clear() {
         try
         {
+            /*
+
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+
+
+            */
+            
             final String os = System.getProperty("os.name");
 
             if (os.contains("Windows"))
             {
-                Runtime.getRuntime().exec("cls");
+                // Runtime.getRuntime().exec("cls");
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             }
             else
             {
-                Runtime.getRuntime().exec("clear");
+                // Runtime.getRuntime().exec("clear");
+                // System.out.print("\033\143");
+                System.out.print("\033[H\033[2J");  
+                System.out.flush();
+                
             }
         }
         catch (final Exception e)
